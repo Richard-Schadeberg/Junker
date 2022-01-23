13657e4b39a19d71b1068ba05ba4de518bd3fc77
Richard-Schadeberg <Richard.Schadeberg@student.uts.edu.au>
2022-01-23 11:19:38 +1100

    Define.S bugfix

--- Assets/Scripts/Animation/AnimationHandler.cs
+++ Assets/Scripts/Animation/AnimationHandler.cs
@@ -7,25 +7,13 @@ public class AnimationHandler {
     float timeNextFire=0;
     // calling Animate() will queue a card motion
     // this motion will reflect the gamestate at the time the animation was queued
-    public static void Animate(Card card,GameAction action) {Game.S.animationHandler._Animate(card,action);}
-    public void _Animate(Card card,GameAction action) {
+    public static void Animate(Card card,GameAction action) {
         // No queueing animations during reversible actions
         if (Game.S.ReversibleMode) return;
-        PackFor(CardAnimation.GoalZone(action,card),CardAnimation.OriginZone(action,card));
-        CardAnimation animation = new CardAnimation(card,action);
-        animationQueue.Enqueue(animation);
-        // PackFor(animation);
+        Game.S.animationHandler.animationQueue.Enqueue(new CardAnimation(card,action));
     }
-    void PackFor(Zone goal,Zone origin) {
-        PackZone(goal);
-        // don't repack hand when cards leave it, so players can accurately click on cards in sequence
-        if (origin!=Zone.Hand) PackZone(origin);
-    }
-    public void PackZone(Zone zone) {
-        foreach (Card card in ZoneTracker.GetCards(zone)) {
-            animationQueue.Enqueue(new CardAnimation(card,GameAction.Repacking));
-        }
-    }
+    public static void PackCards(IEnumerable<Card> cards) {foreach (Card card in cards) {Animate(card,GameAction.Repacking);}}
+    public static void PackZone(Zone zone) {PackCards(ZoneTracker.GetCards(zone));}
     public void AnimateInstant(Card card,GameAction action) {
         BoundsUtil.SetBounds(card,card.bounds);
     }
